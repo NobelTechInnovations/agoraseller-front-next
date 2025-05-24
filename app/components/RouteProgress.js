849549e07c45694b127150f99a557282fe994653
Kartik Maandothiya <kartikya27@gmail.com>
2025-05-24 22:14:00 +0530

    design change

--- app/components/RouteProgress.js
+++ app/components/RouteProgress.js
@@ -1,16 +1,35 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import NProgress from 'nprogress';
+
+// Configure NProgress
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 500,
+  showSpinner: false
+});
 
 export default function RouteProgress() {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    // Start progress bar
     NProgress.start();
-    NProgress.done();
-  }, [pathname]);
+
+    // Simulate a delay to show loading state
+    const timer = setTimeout(() => {
+      NProgress.done();
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+  }, [pathname, searchParams]);
 
   return null;
 }
